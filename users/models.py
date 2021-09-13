db8f10b1b66f157aa404c4d37c7cf930e452a51b
eduardogpg <eduardo78d@gmail.com>
2021-09-12 21:21:41 -0500

    Merge

--- users/models.py
+++ users/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     receive_emails = models.BooleanField(default=True)
-    permision_level = models.IntegerField(default=1, null=False, blank=False)
+    permission_level = models.IntegerField(default=1, null=False, blank=False)
 
     def __str__(self):
         return self.username
 
     def admin(self):
-        return self.permision_level > 3
+        return self.permission_level > 3
